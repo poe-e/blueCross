6e7e1b778caef6987a4c35da0691ce5f4b126478
Poe <pwas123@gmail.com>
2023-03-04 14:42:21 -0500

    passed the search parameter to mainpage with navigate state in order to call a new api request with the searched input

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,24 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import NavBar from "./NavBar";
 import RecipeList from "./RecipeList";
 
 function MainPage(){
+
+    const location = useLocation();
+    const searchQuery = location.state?.searchQuery;
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(searchQuery){
+            navigate('/', { state: { searchQuery: ''}});
+        }
+    });
+
     return(
         <>
             <NavBar></NavBar>
-            <RecipeList></RecipeList>
+            <RecipeList searchQuery={searchQuery}></RecipeList>
         </>
         
 
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -3,29 +3,35 @@ import React, { useState, useEffect } from "react";
 import RecipePost from "./RecipePost";
 import useFetch from "../hooks/useFetch";
 
-function RecipeList(){
+function RecipeList({searchQuery}){
 
     const [curPage, setCurPage] = useState(1);
     const [curPageSize, setCurPageSize] = useState(5);
     const [currentPageData, setCurrentPageData] = useState(null);
 
     const { data, error } = useFetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=d2d83e91f8e940628f5d62a3041fa480');
+    
+
+    console.log('serach!!!!!!: '+searchQuery)
 
 
     return(
         <>
             {error && <p style={{paddingLeft: '2%'}}>Something went wrong, may try refreshing?</p>}
             <Pagnation/>
-            <ul>
-            { data && data.results.map(recipe => {
-                    return <RecipePost 
-                        key={recipe.id}
-                        id={recipe.id}
-                        title={recipe.title}
-                        image={recipe.image}
-                    />
-            })}
-            </ul>
+            <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                <ul >
+                { data && data.results.map(recipe => {
+                        return <RecipePost 
+                            key={recipe.id}
+                            id={recipe.id}
+                            title={recipe.title}
+                            image={recipe.image}
+                        />
+                })}
+                </ul>
+            </div>
+
         </>
     );
 }
