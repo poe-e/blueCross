378d6cf744bda4c6fc3cb3de4363d83c034ffde6
Poe <pwas123@gmail.com>
2023-03-14 17:05:47 -0400

    fixing spaces

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -10,7 +10,7 @@ function RecipeList({searchQuery}){
     const [currentPaginationData, setCurrentPaginationData] = useState([]);
     const [filterValue, setFilterValue] = useState([]);
     const [url, setUrl] = useState('https://api.spoonacular.com/recipes/complexSearch?apiKey=b04c3f5a80ab4b0288f03fe99ac7dd36&number=50')
-    const curPageSize = 5;
+    const curPageSize = 6;
 
     const handleFilterChange  = (value, checked) => {
         setFilterValue(prevFilterValue => {
@@ -50,10 +50,9 @@ function RecipeList({searchQuery}){
 
     return(
         <>
-            <FilterMenu onFilterChange={handleFilterChange}/>
-            <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems:"center", width:"98vw", height:"85vh"}}>
-                {/* <ul style={{listStyleType:"none;", padding:"0;"}}> */}
-                <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-evenly", height:"80%"}}>
+            {/* <FilterMenu onFilterChange={handleFilterChange}/> */}
+            <div style={{display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems:"center", height:"88vh"}}>
+                <div style={{display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-evenly"}}>
                 {currentPaginationData && currentPaginationData.map((recipe) => {
                             return (
                                 <RecipePost
@@ -66,10 +65,7 @@ function RecipeList({searchQuery}){
                     })
                 }
                 </div>
-
-                {/* </ul> */}
-            </div>
-            {data && (
+                {data && (
                 <Pagination 
                     currentPage={curPage}
                     totalCount={data.results.length}
@@ -77,6 +73,7 @@ function RecipeList({searchQuery}){
                     onPageChange={updatePage}
                 />
                 )}
+            </div>
 
         </>
     );
